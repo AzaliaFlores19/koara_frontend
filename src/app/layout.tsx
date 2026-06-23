a74e5c105650b55881493b8c79268acf0f84d6e0
Liaf21 <liaf21ramirez@gmail.com>
2026-06-22 22:12:39 -0600

    fix: hydrate tree fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
-      <body className="no-scrollbar antialiased bg-koara-bg">
+      <body className="no-scrollbar antialiased bg-koara-bg" suppressHydrationWarning>
         <AuthGuard>
           {children}
         </AuthGuard>
